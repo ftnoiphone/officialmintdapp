d1c28ee7e317e387b2f8bb0a7e023af031f6ed92
ftnoiphone <107076606+ftnoiphone@users.noreply.github.com>
2022-06-08 13:47:48 -0400

    Update App.js

--- nft-minting-app-main/src/App.js
+++ nft-minting-app-main/src/App.js
@@ -130,7 +130,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .publicMint(blockchain.account, mintAmount)
+      .publicMint(blockchain.account)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
