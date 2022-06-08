28625bef841c6fb177fe94e32622143450c8f30a
ftnoiphone <107076606+ftnoiphone@users.noreply.github.com>
2022-06-08 00:51:17 -0400

    Update App.js

--- nft-minting-app-main/src/App.js
+++ nft-minting-app-main/src/App.js
@@ -162,8 +162,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 3) {
-      newMintAmount = 3;
+    if (newMintAmount > 20) {
+      newMintAmount = 20;
     }
     setMintAmount(newMintAmount);
   };
